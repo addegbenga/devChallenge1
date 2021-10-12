4b9c2f20e25e3c8edbbaca904366b875e2250050
addegbenga <addegbenga@gmail.com>
2021-10-12 14:26:35 -0400

    changes

--- app.js
+++ app.js
@@ -12,20 +12,14 @@ dotenv.config()
 CONNECTDB()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors('*'))
 app.use(morgan('tiny'))
 
 app.use('/v1', require('./src/routes/v1'))
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'))
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
 
 const PORT = 5000 || process.env.PORT
 app.listen(PORT, () => console.log('connected to Localhost'))
